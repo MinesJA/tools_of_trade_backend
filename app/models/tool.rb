e5a42e7d493649bbbdcd756972261035df86dab4
Jonathan Mines <minesja@gmail.com>
2018-09-15 14:33:28 -0400

    Added comments. Look for .tools_from_tags

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -17,39 +17,12 @@ class Tool < ApplicationRecord
 
   def self.tools_from_tags(tag_string)
     # need to find sql query that returns all rows of Tools that have ALL tag instances sent in. Exclusive
-    # Having a little trouble with an ActiveRecord Query and was wondering if anyone had any ideas. I have a many-to-many between Tools and Tags. Given x unique tag names, is there a query I can run that will give me all the tools that have a relationship to each matching tag (not any matching tag).
-    #
-    # In other words, running @tag.tools for each tag will give me an AR collection of tools but I only want the tools that exist in each collection. So I guess Exclusive not Inclusive.
-    #
-    # Discovered that this `Tool.joins(:tags).where({tags: {name: ["tag_name", "different_tag_name"}})` will give me every tool that has a relationship to any of the tags. Does anyone know how to make that return tools that have a relationship to all of the tags?
 
-    # SELECT  "tools".* FROM "tools" INNER JOIN "tag_tools" ON "tag_tools"."tool_id" = "tools"."id" INNER JOIN "tags" ON "tags"."id" = "tag_tools"."tag_id" WHERE "tags"."name" = $1 LIMIT $2  [["name", "fonts"], ["LIMIT", 11]]
-    # [#<Tool id: 249, name: "Font Pair", description: "Font Pair is a great tool that lets your pair Goog...", url: "https://fontpair.co/", upvotes: 0, downvotes: 0, created_at: "2018-09-05 23:41:20", updated_at: "2018-09-05 23:41:20">, #<Tool id: 250, name: "Typ.io", description: "A really rich and cool font discovery tool. Typ.io...", url: "http://typ.io/", upvotes: 0, downvotes: 0, created_at: "2018-09-05 23:41:20", updated_at: "2018-09-05 23:41:20">]>
-
-    # Tag.where(name: tags_array).map do |tag|
-    #   tag.tools
-    # end.flatten.uniq
-
-
-    #
+    # This will give me an array of arrays of tools, each array cooresponds to a return from a different tags tools.
     # array_of_arrays_tools = tags_array.map do |tag|
     #     Tool.joins(:tags).where({tags: {name: tag}})
     # end
-    #
-    # tools = []
-    #
-    # array_of_arrays_tools.each do |array|
-    #   tools.push(array & nextArray)
-    #
-    #
-    # end
-    #
-    # array_all_tools = Tool.joins(:tags).where({tags: {name: tags_array}})
-    # byebug
-    # array_all_tools.collect do |tool|
-    #   byebug
-    #   array_all_tools.count(tool) == tags_array.count
-    # end
+    # Thi
 
 
     tags_array = tag_string.split(",")
@@ -59,7 +32,6 @@ class Tool < ApplicationRecord
     array_all_tools = Tool.joins(:tags).where({tags: {name: tags_array}})
 
     array_all_tools.select {|tool| (tool.tags & tags).count == tags.count}.uniq
-
   end
 
 
